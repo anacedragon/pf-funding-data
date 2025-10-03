c9552bdcf638ba24e97392fc5d651f5ed8621c2f
J <juliatigerbeall@gmail.com>
2025-10-02 21:44:34 -0400

    funds Thu Oct  2 21:44:34 EDT 2025

--- auto-backups.sh
+++ auto-backups.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# define variables
+home=/home/ubuntu
+
+# copy files from tracker
+cp -rup $home/pf-funding-tracker/output/funds* $home/pf-funding-data
+
 # git backup
 git add --all
 git commit --message="funds `date`"
